24df04bfa495a0926ce5bbec9fb38c20a12a6c13
zhousw <1006923900@qq.com>
2020-06-13 13:42:58 +0800

    增加类型

--- src/pages/table/index.tsx
+++ src/pages/table/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from './index.less';
 import { Button, Table, Tag, Space } from 'antd';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 export default () => {
   const columns = [
     {
@@ -28,26 +28,31 @@ export default () => {
     },
   ];
 
-  const [userData, setDatas] = useState([]);
+  interface Data {
+    id: string;
+    name: string;
+    age: number;
+    address: string;
+  }
+  const [userData, setDatas] = useState<Data[]>([] as Data[]);
 
   useEffect(() => {
+    //没有回调会一直执行
     getUserData();
-  });
+  }, []);
 
   const getUserData = () => {
-    axios.get('/api/tableList').then(
-      (res: any) => {
-        console.log(res);
-        setDatas(res.data);
-      },
-      err => {},
-    );
+    axios.get('/api/tableList').then((res: AxiosResponse<Data[]>) => {
+      setDatas(res.data);
+    });
   };
-
   return (
     <div>
-      <Table columns={columns} dataSource={userData}></Table>
-      fdsfdf
+      <Table
+        columns={columns}
+        dataSource={userData}
+        rowKey={(userData: { id: string }) => userData.id}
+      ></Table>
     </div>
   );
 };
